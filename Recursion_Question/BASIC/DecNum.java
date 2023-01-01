15eb9d61258c324d9086d23bd761cf8477c7069f
AdityaGoel0320 <112582770+AdityaGoel0320@users.noreply.github.com>
2023-01-01 18:36:12 +0530

    Add files via upload

---
+++ Recursion_Question/BASIC/DecNum.java
@@ -0,0 +1,25 @@
+package Recursion_Question.BASIC;
+
+//want to print like  5 4 3 2 1
+
+public class DecNum {
+    static void decnum(int n){
+        //base case
+        if(n==1){
+            System.out.println(1);
+            return ; 
+        }
+
+        // my work  /self work
+        System.out.println(n);
+
+        //recusion work bharsoa rakhp iss pe
+        decnum(n-1);
+    }
+    public static void main(String[] args) {
+
+        decnum(5);
+        
+    }
+    
+}
